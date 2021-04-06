e3081f8710f6bc23cb99889676a2d56a373fd8fa
Cedric VICO <cedric.vico@hotmail.com>
2021-04-06 14:31:22 +0200

    Setup

--- Assets/Scripts/ThunderBoxBehaviour.cs
+++ Assets/Scripts/ThunderBoxBehaviour.cs
@@ -10,6 +10,10 @@ public class ThunderBoxBehaviour : MonoBehaviour
     public GameObject @object;
     BackgroundBehaviour backgroundObject;
     Platform platform;
+    public GameObject Flash;
+    int timer = 40;
+    bool flash = false;
+    [SerializeField]
     Animator eclair;
 
     private void Awake()
@@ -28,23 +32,39 @@ public class ThunderBoxBehaviour : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerBehaviour player = collision.GetComponent<PlayerBehaviour>();
-        eclair = GetComponent<Animator>();
         if (player != null && background)
         {
             if(backgroundObject.random)
             {
                 backgroundObject.ChangeBKColorState((BackgroundBehaviour.BackGroundColorState)backgroundObject.RandomBK());
                 eclair.SetTrigger("Trigger");
+                Flash.SetActive(true);
+                flash = true;
             }
             else
             {
                 backgroundObject.ChangeBKColorState((BackgroundBehaviour.BackGroundColorState)color);
                 eclair.SetTrigger("Trigger");
+                Flash.SetActive(true);
+                flash = true;
             }
         }
         else if(player != null)
         {
             platform.ChangePlatformState((Platform.PlatformState)platformType);
+        }
+    }
+
+    private void Update()
+    {
+        if(flash == true)
+        {
+            timer -= 1;
+            if (timer < 1)
+            {
+                Flash.SetActive(false);
+                flash = false;
+            }
         }
     }
 }
